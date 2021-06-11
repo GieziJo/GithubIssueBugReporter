5b8a5a89f00e8e147a402675bd1b94e68aa7f78d
Giezi <bob.giezi@gmail.com>
2021-06-11 09:12:00 +0200

    better way to manage access tokens with json files

--- Assets/Scripts/AccessToken.cs
+++ Assets/Scripts/AccessToken.cs
@@ -1,4 +1,30 @@
-﻿public  struct AccessToken
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using UnityEngine;
+
+public static class AccessToken
 {
-    public const string TOKEN = "";
+    public static string BUG_REPO_TOKEN => ReturnBugRepoToken();
+    public static string IMAGE_REPO_TOKEN => ReturnImageRepoToken();
+
+    private static string bug_repo_token;
+    private static string image_repo_token;
+
+    static AccessToken()
+    {
+        Initialize();
+    }
+
+    private static string ReturnBugRepoToken() => bug_repo_token;
+
+    private static string ReturnImageRepoToken() => image_repo_token;
+
+    private static void Initialize()
+    {
+        if (!(bug_repo_token == null || image_repo_token == null))
+            return;
+        Dictionary<string, string> tokens = JsonConvert.DeserializeObject<Dictionary<string, string>>(Resources.Load<TextAsset>("GieziTools.Tokens/GithubAccessTokens").ToString());
+        bug_repo_token = tokens["github-repo-bug-tracker-token"];
+        bug_repo_token = tokens["github-repo-bug-images-token"];
+    }
 }
--- Assets/Scripts/GithubBugReporter.cs
+++ Assets/Scripts/GithubBugReporter.cs
@@ -11,10 +11,15 @@ public class GithubBugReporter : MonoBehaviour
 
     void ReportBug()
     {
-        var client = new GitHubClient(new ProductHeaderValue("EarlyCoffee.Games.Hermit"));
-        var basicAuth = new Credentials(AccessToken.TOKEN);
-        client.Credentials = basicAuth;
-        WaitForAnswer(client);
+        // new AccessToken("test1", "test2");
+        
+        Debug.Log(AccessToken.BUG_REPO_TOKEN);
+        
+        // var client = new GitHubClient(new ProductHeaderValue("EarlyCoffee.Games.Hermit"));
+
+        // var basicAuth = new Credentials(AccessToken.BUG_REPO_TOKEN);
+        // client.Credentials = basicAuth;
+        // WaitForAnswer(client);
     }
 
     private async void WaitForAnswer(GitHubClient client)
